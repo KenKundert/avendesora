ca1a151726f6968c44669f36502403a657df2aba
Ken Kundert <thenurd@nurdletech.com>
2016-07-08 01:34:05 -0700

    Bump version: 0.1.4 → 0.1.5

--- avendesora/__init__.py
+++ avendesora/__init__.py
@@ -13,4 +13,4 @@ from .recognizers import (
 
 from .gpg import GPG
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name='avendesora',
-    version='0.1.4',
+    version='0.1.5',
     author='Ken Kundert and Kale Kundert',
     author_email='avendesora@nurdletech.com',
     description='An XKCD-style password generator.',
