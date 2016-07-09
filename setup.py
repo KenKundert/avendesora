381b35bb9fce0f31a7500ee5f389095d12bea94b
Ken Kundert <thenurd@nurdletech.com>
2016-07-09 00:54:33 -0700

    Bump version: 0.1.10 → 0.1.11

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name='avendesora',
-    version='0.1.10',
+    version='0.1.11',
     author='Ken Kundert and Kale Kundert',
     author_email='avendesora@nurdletech.com',
     description='An XKCD-style password generator.',
