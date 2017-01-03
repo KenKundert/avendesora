439174e9c6c8fde217bcc29c7b3f8b7ee11eb94e
Ken Kundert <ken@theKunderts.net>
2017-01-03 00:49:00 -0800

    bump patch

--- avendesora/__init__.py
+++ avendesora/__init__.py
@@ -19,5 +19,5 @@ from .recognize import (
 from .generator import PasswordGenerator
 from inform import Error as PasswordError
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 __released__ = '2017-01-03'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name='avendesora',
-    version='1.0.2',
+    version='1.0.3',
     author='Ken Kundert and Kale Kundert',
     author_email='avendesora@nurdletech.com',
     description='An XKCD-style password generator.',
