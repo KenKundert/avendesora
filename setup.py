075eda0993d7f7d51b1ddda73026fb77b71e38fa
Ken Kundert <ken@theKunderts.net>
2019-07-29 21:21:56 -0700

    Remove support python3.4.

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
