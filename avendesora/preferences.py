ec0c9c1944875d0b1ac20baf3509fed45bb06ac9
Ken Kundert <ken@theKunderts.net>
2020-01-01 00:39:40 -0800

    Update hashes.

--- avendesora/preferences.py
+++ avendesora/preferences.py
@@ -31,9 +31,9 @@ NONCONFIG_SETTINGS = {
     'cache_dir': user_cache_dir('avendesora'),
     'default_accounts_file': 'accounts.gpg',
     'default_stealth_accounts_file': 'stealth_accounts',
-    'charsets_hash': '0120b695fb247d4993b1595ab087cc15',
-    'dict_hash': '11fe5bc734f4a956c37d7cb3da16ab3f',
-    'secrets_hash': 'c5721ab26ed8e36eb3d07ac886dde931',
+    'charsets_hash': 'a055240c4c498e1470f5f3e80b3ec599',
+    'dict_hash': '5dbabd4114eae520c1de8963a8b8d09d',
+    'secrets_hash': 'f7a47122864464703212663a407e4cb6',
     'discard_logfile': False,
     'commonly_mistaken_attributes': {
         'url': 'urls',
