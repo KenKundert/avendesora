3df0b59fcc6b9664e16aad0fd6a1d5e0ec5beb20
Ken Kundert <ken@theKunderts.net>
2020-01-31 16:59:41 -0800

    Another attempt at getting RTD working.

--- setup.py
+++ setup.py
@@ -1,11 +1,46 @@
 #!/usr/bin/env python
 
+# imports {{{1
 from setuptools import setup
 from codecs import open
+import os
 
+# build the description {{{1
 with open('README.rst', encoding='utf-8') as file:
     readme = file.read()
 
+# build the installation requirements {{{1
+install_requirements = '''
+    appdirs
+    arrow!=0.14.*
+    cryptography
+    docopt
+    inform>=1.17
+    pygobject
+    python-gnupg>=0.4.4
+        # Be careful.  There's a package called 'gnupg' that's an
+        # incompatible fork of 'python-gnupg'.  If both are installed, the
+        # user will probably have compatibility issues.
+    pyotp
+        # pyotp is optional, it provides one-time-password (OTP) secrets.
+    #scrypt
+        # scrypt is optional. If you install it then Avendesora will offer
+        # it. It is not required because it is little used and installing it
+        # involves compiling C code and so significant additional
+        # dependencies such as gcc.
+'''
+install_requires = []
+for line in install_requirements.splitlines():
+    code, _, comment = line.partition('#')
+    requirement = code.strip()
+    if requirement:
+        if requirement.startswith('pygobject') and 'READTHEDOCS' in os.environ:
+            pass
+        else:
+            install_requires.append(requirement)
+
+
+# call setup {{{1
 setup(
     name = 'avendesora',
     version = '1.16.4',
@@ -20,25 +55,7 @@ setup(
     entry_points = {
         'console_scripts': ['avendesora = avendesora.main:main'],
     },
-    install_requires = [
-        'appdirs',
-        'arrow!=0.14.*',
-        'cryptography',
-        'docopt',
-        'inform>=1.17',
-        'pygobject',
-        'python-gnupg>=0.4.4',
-            # Be careful.  There's a package called 'gnupg' that's an
-            # incompatible fork of 'python-gnupg'.  If both are installed, the
-            # user will probably have compatibility issues.
-        'pyotp',
-            # pyotp is optional, it provides one-time-password (OTP) secrets.
-        #'scrypt',
-            # scrypt is optional. If you install it then Avendesora will offer
-            # it. It is not required because it is little used and installing it
-            # involves compiling C code and so significant additional
-            # dependencies such as gcc.
-    ],
+    install_requires = install_requires,
     setup_requires = 'pytest-runner>=2.0'.split(),
     tests_require = 'pytest pexpect'.split(),
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
