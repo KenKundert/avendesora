d3adeac3288a0ce98739bb143ca6554449648679
Ken Kundert <ken@theKunderts.net>
2020-06-28 20:08:08 -0700

    Deprecate python 2.7 and 3.5.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     install_requires = install_requires,
     setup_requires = 'pytest-runner>=2.0'.split(),
     tests_require = 'pytest pexpect'.split(),
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
+    python_requires='>=3.6',
     keywords = 'avendesora password XKCD'.split(),
     classifiers = [
         'Development Status :: 5 - Production/Stable',
@@ -68,8 +68,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
