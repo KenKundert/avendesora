752160c30a70fb529f6d80ab427805c645d832c8
Ken Kundert <ken@theKunderts.net>
2021-11-09 10:59:13 -0800

    fix incompatibility between sphinx and docutils

--- setup.py
+++ setup.py
@@ -15,10 +15,6 @@ install_requirements = '''
     arrow!=0.14.*
     cryptography
     docopt
-    docutils!=0.18
-        # not needed directly by Avendesora, instead it a requirement for
-        # Sphinx.  There is some incompatibility between version 0.18 and
-        # the latest versions of Sphinx.
     inform>=1.21
     pygobject
     python-gnupg>=0.4.4
